cdba509dc6f110be03bcc5117e6de08f45336627
alumno3 <villamario7@digitechfp.com>
2026-02-16 16:21:49 +0100

    Initial commit

---
+++ src/main/java/progresa/ventadeproductosygestiondepedidos/VentaDeProductosYGestionDePedidosApplication.java
@@ -0,0 +1,13 @@
+package progresa.ventadeproductosygestiondepedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaDeProductosYGestionDePedidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VentaDeProductosYGestionDePedidosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/progresa/ventadeproductosygestiondepedidos/VentaDeProductosYGestionDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package progresa.ventadeproductosygestiondepedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaDeProductosYGestionDePedidosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
